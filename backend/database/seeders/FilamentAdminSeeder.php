8de99aa3fd4281458d6ff0dc132e5887254c794e
Rahul <rahul@corbelbiz.com>
2024-01-08 15:41:39 +0530

    update

---
+++ backend/database/seeders/FilamentAdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class FilamentAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $filamentMakeUserCommand = new FilamentMakeUserCommand();
+        $reflector = new \ReflectionObject($filamentMakeUserCommand);
+
+        $getUserModel = $reflector->getMethod('getUserModel');
+        $getUserModel->setAccessible(true);
+        $getUserModel->invoke($filamentMakeUserCommand)::create([
+            'name' => 'admin',
+            'email' => 'admin@furn.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
